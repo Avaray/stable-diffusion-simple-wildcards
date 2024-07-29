db22805eb5b263315e88ea4f25d11f5a86acfa9f
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-29 17:40:56 +0200

    Update update_docs.ts

--- scripts/update_docs.ts
+++ scripts/update_docs.ts
@@ -3,9 +3,16 @@ console.log('Starting');
 // import { $ } from "bun"
 import { readdir } from "node:fs/promises";
 
+console.log(Bun.env);
+
+// const PWD = Bun.env.PWD;
+
+process.exit(0);
+
 const wildcards = await readdir('../wildcards');
 
-const repoName = Bun.env.GITHUB_REPOSITORYQ?.split('/')[1];
-const repoOwner = Bun.env.GITHUB_REPOSITORYC?.split('/')[0];
+const repoName = Bun.env.GITHUB_REPOSITORY?.split('/')[1];
+const repoOwner = Bun.env.GITHUB_REPOSITORY?.split('/')[0];
 const branchName = Bun.env.GITHUB_REF_NAME;
 const apiURL = Bun.env.GITHUB_API_URL;
+
