18805bff2a1921dd57f34eb981da63c539b9984a
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-29 12:35:39 +0200

    ...

--- scripts/update-docs.ts
+++ scripts/update-docs.ts
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+console.log('Starting');
+
+import { readdir } from "node:fs/promises";
+
+import { $ } from "bun"
+
+const wildcards = await readdir('../wildcards');
+
+
+console.log(wildcards);
