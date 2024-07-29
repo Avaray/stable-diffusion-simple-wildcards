5365e8128e6e49410195f32b934c9d3dafecaddc
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-29 17:36:00 +0200

    Update update_docs.ts

--- scripts/update_docs.ts
+++ scripts/update_docs.ts
@@ -1,12 +1,11 @@
-// console.log('Starting');
-
-// import { readdir } from "node:fs/promises";
+console.log('Starting');
 
 // import { $ } from "bun"
+import { readdir } from "node:fs/promises";
 
-// const wildcards = await readdir('../wildcards');
+const wildcards = await readdir('../wildcards');
 
-
-// console.log(wildcards);
-
-console.log(Bun.env);
+const repoName = Bun.env.GITHUB_REPOSITORYQ?.split('/')[1];
+const repoOwner = Bun.env.GITHUB_REPOSITORYC?.split('/')[0];
+const branchName = Bun.env.GITHUB_REF_NAME;
+const apiURL = Bun.env.GITHUB_API_URL;
