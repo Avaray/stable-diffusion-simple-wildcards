c338bc244b117cf42c1dc39d7de807945e3f0b79
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-29 17:22:26 +0200

    Update update_docs.ts

--- scripts/update_docs.ts
+++ scripts/update_docs.ts
@@ -1,10 +1,12 @@
-console.log('Starting');
+// console.log('Starting');
 
-import { readdir } from "node:fs/promises";
+// import { readdir } from "node:fs/promises";
 
-import { $ } from "bun"
+// import { $ } from "bun"
 
-const wildcards = await readdir('../wildcards');
+// const wildcards = await readdir('../wildcards');
 
 
-console.log(wildcards);
+// console.log(wildcards);
+
+console.log(Bun.env);
