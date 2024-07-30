3271c6a6aa8c857498e0f43608e97166cfa79fa0
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-30 19:41:11 +0200

    Update update_docs.ts

--- scripts/update_docs.ts
+++ scripts/update_docs.ts
@@ -1,19 +1,77 @@
-console.log('Starting');
+// console.log('Starting');
 
-// import { $ } from "bun"
-import { readdir } from "node:fs/promises";
+// console.log(`meta path: ${import.meta.path}`);
+// console.log(`meta dir: ${import.meta.dir}`);
 
-const PWD = Bun.env.PWD;
+// https://docs.github.com/en/actions/learn-github-actions/variables
+// https://docs.github.com/en/actions/learn-github-actions/contexts#github-context
 
-const wildcards = await readdir(`${PWD}/wildcards`);
+import { $ } from 'bun';
+import { readdir } from 'node:fs/promises';
+import { urls, automatic, manual } from './commands.ts';
 
-console.log(wildcards);
-console.log(wildcards.length);
+const branches = {
+  sdxl: {
+    name: 'Stable Diffusion XL',
+  },
+  pdxl: {
+    name: 'Pony Diffusion XL',
+  },
+};
 
-const repoName = Bun.env.GITHUB_REPOSITORYQ?.split('/')[1];
-const repoOwner = Bun.env.GITHUB_REPOSITORYC?.split('/')[0];
-const branchName = Bun.env.GITHUB_REF_NAME;
+// if (!Bun.env.GITHUB_REPOSITORY || !Bun.env.GITHUB_REF_NAME || !Bun.env.GITHUB_API_URL || !Bun.env.PWD) {
+//   console.log('This script must be run in a GitHub Action environment');
+//   process.exit(1);
+// }
+
+const repoOwner = Bun.env.GITHUB_REPOSITORY?.split('/')[0];
+const repoName = Bun.env.GITHUB_REPOSITORY?.split('/')[1];
+const branchName = Bun.env.GITHUB_REF_NAME || 'sdxl';
 const apiURL = Bun.env.GITHUB_API_URL;
 
-console.log(repoName);
-console.log(repoOwner);
+const path = Bun.env.GITHUB_REPOSITORY ? Bun.env.PWD : import.meta.dir;
+
+console.log(path);
+
+const wildcards = await readdir(`${path}/../wildcards`);
+const filesList = `${wildcards.map((w) => `- [${w.split('.')[0]}](wildcards/${w})\n`).join('')}\n`;
+
+const downloadMethod = (method: {
+  type: string;
+  tools: string[];
+  commands: string[];
+}) => {
+  const header = `### Download${method.type === 'automatic' ? ' automatically' : ''} with ${method.tools.map((tool) => `[${tool.toUpperCase()}](${urls[tool]})`).join(' and ')}\n\n`;
+  const code = `\`\`\`bash\n${method.commands.join(' && ')}\n\`\`\`\n`;
+
+  return header + code;
+};
+
+const automaticMethods = automatic.map((m) => downloadMethod(m)).join('\n');
+
+const manualMethods = manual.map((m) => downloadMethod(m)).join('\n');
+
+const docsFiles = await readdir(`${path}/../src`);
+
+docsFiles.forEach(async (file) => {
+  const content = await Bun.file(`${path}/../src/${file}`).text();
+  const processed = content
+    .replaceAll('{{filesList}}', filesList)
+    .replaceAll('{{branch}}', branchName)
+    .replaceAll('{{automaticMethods}}', automaticMethods)
+    .replaceAll('{{manualMethods}}', manualMethods)
+    .replaceAll('{{amount}}', wildcards.length.toString())
+    .replace(/^\n\n/gm, '\n');
+
+  if (file === 'README.md') {
+    await Bun.write(`${path}/../${file}`, processed);
+  } else {
+    await Bun.write(`${path}/../docs/${file}`, processed);
+  }
+});
+
+const readme = await Bun.file(`${path}/../README.md`).text();
+
+// await Bun.write(`${PWD}/README.md`, cleanReadme);
+
+console.log('Done');
