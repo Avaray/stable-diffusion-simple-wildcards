d5f5f7bf86e2d7603dcf80ad11f08dce5cdbc7c3
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-30 19:43:40 +0200

    Update commands.ts

--- scripts/commands.ts
+++ scripts/commands.ts
@@ -2,10 +2,12 @@ import data from '../package.json';
 
 // These will come from environment variables in the future
 // Need to figure out how to handle this with GitHub Actions
-const branch = 'sdxl';
+const branch = Bun.env.GITHUB_REF_NAME || 'sdxl';
 const repositoryName = data.name;
 const repositoryUrl = data.repository.url;
 const scriptUrl = `https://raw.githubusercontent.com/${data.Author}/${repositoryName}/${branch}/scripts/download.sh`;
+const archiveUrl = `https://github.com/Avaray/stable-diffusion-simple-wildcards/archive/refs/heads/${branch}.zip`;
+const archiveFilename = 'wildcards.zip';
 
 export const urls = {
   bash: 'https://www.gnu.org/software/bash/',
@@ -19,38 +21,34 @@ export const urls = {
 
 export const automatic = [
   {
+    type: 'automatic',
     tools: ['bash', 'wget'],
-    commands: [`wget -qO- ${scriptUrl} | bash -s -- wget sdxl`],
+    commands: [`wget -qO- ${scriptUrl} | bash -s -- wget ${branch}`],
   },
   {
+    type: 'automatic',
     tools: ['bash', 'aria2c'],
     commands: [
       `aria2c -q --allow-overwrite=true --remove-control-file=true -o dl.sh ${scriptUrl}`,
       'chmod +x dl.sh',
-      './dl.sh aria2c sdxl',
+      `./dl.sh aria2c ${branch}`,
     ],
   },
   {
+    type: 'automatic',
     tools: ['bash', 'curl'],
-    commands: [`curl -s ${scriptUrl} | bash -s -- curl sdxl`],
+    commands: [`curl -s ${scriptUrl} | bash -s -- curl ${branch}`],
   },
-];
+] as { type: string; tools: string[]; commands: string[] }[];
 
 export const manual = [
   {
-    tools: ['wget', 'unzip'],
-    commands: [`wget -qO- ${scriptUrl} | bash -s -- unzip sdxl`],
-  },
-  {
-    tools: ['wget', 'tar'],
-    commands: [`wget -qO- ${scriptUrl} | bash -s -- tar sdxl`],
-  },
-  {
+    type: 'manual',
     tools: ['git'],
     commands: [
-      `git clone --single-branch --branch sdxl ${repositoryUrl}`,
+      `git clone --single-branch --branch ${branch} ${repositoryUrl}`,
       `mv ${repositoryName}/wildcards/*.txt . > /dev/null 2>&1`,
       `rm -rf ${repositoryName}`,
     ],
   },
-];
+] as { type: string; tools: string[]; commands: string[] }[];
