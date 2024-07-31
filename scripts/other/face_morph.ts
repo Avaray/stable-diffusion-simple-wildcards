e4418644516d4e8e70e71cde6ffaeebda24fa026
Avaray <1459553+Avaray@users.noreply.github.com>
2024-07-31 07:37:22 +0200

    had fun

--- scripts/other/face_morph.ts
+++ scripts/other/face_morph.ts
@@ -2,40 +2,134 @@
 // https://www.reddit.com/r/StableDiffusion/comments/1eeflm2/improving_face_variation_in_generations/
 // It outputs crazy long prompt for sd-dynamic-prompts extension
 
-import { readdir } from "node:fs/promises";
+import { readdir } from 'node:fs/promises';
 
-const nationalities = await Bun.file('../../wildcards/nationalities.txt').text();
-const nationalitiesCleaned = nationalities.replace(/[\r\n]+/g, '\n');
-const nationalitiesArray = nationalitiesCleaned.split('\n').filter(nat => nat.length > 0);
+const nationalitiesFile = await Bun.file('../../wildcards/nationalities.txt').text();
+const nationalitiesCleaned = nationalitiesFile.replace(/[\r\n]+/g, '\n');
+const nationalities = nationalitiesCleaned.split('\n').filter((nat) => nat.length > 0);
+
+// Let's extract all names ending with letter 'a' from artists2.txt
+const artistNames = await Bun.file('../../wildcards/artists2.txt').text();
+const artistNamesExtracted = artistNames.match(/^\b\w{2,}a\b/gm);
+const names = Array.from(new Set(artistNamesExtracted));
+
+// prettier-ignore
+const femaleNames = ['Alice', 'Ava', 'Ella', 'Emma', 'Grace', 'Hannah', 'Isabella', 'Lily', 'Olivia', 'Sophia', 'Zoe', 'Abigail', 'Amelia', 'Aria', 'Aurora', 'Bella', 'Camila', 'Charlotte', 'Chloe', 'Claire', 'Elena', 'Elizabeth', 'Emily', 'Eva', 'Evelyn', 'Harper', 'Hazel', 'Layla', 'Leah', 'Lucy', 'Mia', 'Mila', 'Nora', 'Penelope', 'Riley', 'Scarlett', 'Stella', 'Victoria', 'Violet', 'Willow', 'Adelaide', 'Catherine', 'Dorothy', 'Eleanor', 'Frances', 'Genevieve', 'Josephine', 'Margaret', 'Matilda', 'Rosemary', 'Aaliyah', 'Brielle', 'Cora', 'Emery', 'Jasmine', 'Kinsley', 'Luna', 'Maya', 'Nyla', 'Sienna', 'Azura', 'Calliope', 'Dahlia', 'Elowen', 'Indigo', 'Juniper', 'Seraphina', 'Talia', 'Zinnia', 'Anya', 'Bianca', 'Chiara', 'Freya', 'Ines', 'Leila', 'Niamh', 'Saoirse', 'Yara', 'Autumn', 'Clover', 'Daisy', 'Ivy', 'Juniper', 'Marigold', 'Poppy', 'Sage', 'Sky', 'Wren', 'Agnes', 'Beatrice', 'Clara', 'Edith', 'Florence', 'Mabel', 'Nora', 'Opal', 'Sylvia', 'Winifred', 'Ari', 'Bea', 'Eve', 'Gia', 'Joy', 'Liv', 'Mae', 'Nia', 'Pia', 'Zia'];
+const unwantedNames = ['Grandma', 'Tetsuya', 'Tsubasa'];
+const allNames = [...names, ...femaleNames].filter((name) => !unwantedNames.includes(name)).sort();
 
 const data: { [key: string]: string[] } = {
-  "nationalities": nationalitiesArray,
-  "female_names": ["Alice", "Ava", "Ella", "Emma", "Grace", "Hannah", "Isabella", "Lily", "Olivia", "Sophia", "Zoe",
-    "Abigail", "Amelia", "Aria", "Aurora", "Bella", "Camila", "Charlotte", "Chloe", "Claire", "Elena",
-    "Elizabeth", "Emily", "Eva", "Evelyn", "Harper", "Hazel", "Layla", "Leah", "Lucy", "Mia", "Mila",
-    "Nora", "Penelope", "Riley", "Scarlett", "Stella", "Victoria", "Violet", "Willow"],
-  "face": ["round", "oval", "square", "heart-shaped", "diamond-shaped"],
-  "forehead": ["high", "low", "prominent", "receding"],
-  "eyebrows": ["arched", "straight", "thick", "thin", "bushy"],
-  "nose": ["long", "short", "wide", "narrow", "upturned", "downturned"],
-  "lips": ["full", "thin", "heart-shaped", "bow-shaped"],
-  "chin": ["strong", "weak", "square", "pointed", "round"],
-  "cheekbones": ["high", "low", "prominent", "subtle"],
-  "ears": ["small", "large", "protruding", "close-set"],
-  // "freckles": ["light", "moderate", "heavy"],
-  // "laugh_lines": ["subtle", "prominent"],
-  // "skin_texture": ["smooth", "slightly wrinkled", "wrinkled"]
-}
+  nationalities: nationalities,
+  names: allNames,
+  face: [
+    'round',
+    'oval',
+    'square',
+    'heart-shaped',
+    'diamond-shaped',
+    'long',
+    'triangular',
+    'oblong',
+    'rectangular',
+  ],
+  forehead: [
+    'high',
+    'low',
+    'prominent',
+    'receding',
+    'wide',
+    'narrow',
+    'smooth',
+    'wrinkled',
+    'forehead with bangs',
+  ],
+  eyebrows: [
+    'arched',
+    'straight',
+    'thick',
+    'thin',
+    'bushy',
+    'curved',
+    'sparse',
+    'angled',
+    'straight and long',
+  ],
+  nose: [
+    'long',
+    'short',
+    'wide',
+    'narrow',
+    'upturned',
+    'downturned',
+    'button',
+    'aquiline',
+    'flat',
+    'crooked',
+  ],
+  lips: [
+    'full',
+    'thin',
+    'heart-shaped',
+    'bow-shaped',
+    'wide',
+    'narrow',
+    'plump',
+    'asymmetrical',
+    'defined',
+  ],
+  chin: [
+    'strong',
+    'weak',
+    'square',
+    'pointed',
+    'round',
+    'recessed',
+    'double',
+    'prominent',
+    'dimpled',
+  ],
+  cheekbones: [
+    'high',
+    'low',
+    'prominent',
+    'subtle',
+    'angular',
+    'rounded',
+    'flat',
+    'wide-set',
+    'narrow',
+  ],
+  ears: [
+    'small',
+    'large',
+    'protruding',
+    'close-set',
+    'pointed',
+    'round',
+    'asymmetrical',
+    'dropped',
+    'fleshy',
+  ],
+  // 'freckles': ['light', 'moderate', 'heavy'],
+  // 'laugh_lines': ['subtle', 'prominent'],
+  // 'skin_texture': ['smooth', 'slightly wrinkled', 'wrinkled']
+};
 
-const variables = Object.keys(data).map(key => {
-  const values = data[key].join('|');
-  return `\${${key}={${values}}}`;
-}).join(' ');
+const variables = Object.keys(data)
+  .map((key) => {
+    const values = data[key].join('|');
+    return `\${${key}={${values}}}`;
+  })
+  .join(' ');
 
-const prompts = Object.keys(data).map(key => {
-  if (key === 'nationalities') return '';
-  return `\${${key}} ${key.replace('_', ' ')}`;
-}).join(', ');
+const prompts = Object.keys(data)
+  .map((key) => {
+    if (key === 'nationalities') return '';
+    if (key === 'female_names') return `\${${key}}`;
+    return `\${${key}} ${key.replace('_', ' ')}`;
+  })
+  .filter((prompt) => prompt !== '')
+  .join(', ');
 
 const combined = `${variables} ${prompts}, [\${nationalities}|\${nationalities}]`;
 
