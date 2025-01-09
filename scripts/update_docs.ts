40875c6e0941939cc2bd5da6206eecd3c9de94f7
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-09 02:16:08 +0100

    Update update_docs.ts

--- scripts/update_docs.ts
+++ scripts/update_docs.ts
@@ -71,12 +71,11 @@ const emptyLinesInMarkdownLists = new RegExp("(?<=^- .*\n)\\s*\n(?=- )", "gm");
 
 const replaceNonBranchContent = (content: string) => {
   const branch = branchName === "sdxl" ? "pdxl" : "sdxl";
-
   const regex = new RegExp(
     `<!-- ${branch} -->(.*?)<!-- \/${branch} -->`,
     "gms",
   );
-  return content.replace(regex, "").replace(/(?<=^\s*-\s.*\n)^\s*$\n?/, "");
+  return content.replace(regex, "").replace(/\n<!-- \w+ -->/gms, "").replace(/(?<=^\s*-\s.*\n)^\s*$\n?/, "");
 };
 
 const automaticMethods = automatic.map((m) => downloadMethod(m)).join("\n");
