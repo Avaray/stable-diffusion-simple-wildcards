c0dffb09fb72cdda73b69432d02e1b5cd938c0fa
Avaray <1459553+Avaray@users.noreply.github.com>
2025-01-09 02:29:04 +0100

    Update update_docs.ts

--- scripts/update_docs.ts
+++ scripts/update_docs.ts
@@ -75,7 +75,7 @@ const replaceNonBranchContent = (content: string) => {
     `<!-- ${branch} -->(.*?)<!-- \/${branch} -->`,
     "gms",
   );
-  return content.replace(regex, "").replace(/\n<!-- \W?\w+ -->/gms, "").replace(/(?<=^\s*-\s.*\n)^\s*$/gm, "");
+  return content.replace(regex, "").replace(/\n<!-- \W?\w+ -->/gms, "").replace(/(?<=^-.*\n)^\s*$\n(?=^-)/gms, "");
 };
 
 const automaticMethods = automatic.map((m) => downloadMethod(m)).join("\n");
